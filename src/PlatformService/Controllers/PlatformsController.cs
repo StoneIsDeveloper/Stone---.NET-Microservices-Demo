56eb0150ece2a239a137c035a4db0076a883fcad
stoneisdeveloper <m18146611430@163.com>
2023-07-03 23:09:24 +0800

    add api method CreatePlatform

--- src/PlatformService/Controllers/PlatformsController.cs
+++ src/PlatformService/Controllers/PlatformsController.cs
@@ -2,6 +2,7 @@ using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using PlatformService.Data;
 using PlatformService.Dtos;
+using PlatformService.Models;
 
 namespace PlatformService.Controllers
 {
@@ -31,7 +32,7 @@ namespace PlatformService.Controllers
         }
 
        
-        [HttpGet("{id}")]
+        [HttpGet("{id}", Name = "GetPlatformById")]
         public ActionResult<PlatformReadDto> GetPlatformById(int id)
         {
             var platformItem = _repository.GetPlatformById(id);
@@ -42,6 +43,18 @@ namespace PlatformService.Controllers
 
             return NotFound();
             
+        }
+        
+         [HttpPost]
+        public ActionResult<PlatformReadDto> CreatePlatform(PlatformCreateDto platformDto)
+        {
+            var platformModel = _mapper.Map<Platform>(platformDto);
+            _repository.CreatePlatform(platformModel);
+            _repository.SaveChanges();
+
+            var platformReadDto = _mapper.Map<PlatformReadDto>(platformModel);
+
+            return CreatedAtRoute(nameof(GetPlatformById), new { Id = platformReadDto.Id}, platformReadDto );
         }
 
 
